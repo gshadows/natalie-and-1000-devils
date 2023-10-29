bdc3a7e84708b40fcb2171b3abd3509125744445
G-Shadow <18046412+gshadows@users.noreply.github.com>
2023-10-29 20:14:40 +0300

    Fix GameUI bug overlapping menu.

--- menu/game_ui.gd
+++ menu/game_ui.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	Game.paused.connect(func(): visible = false)
+	Game.continued.connect(func(): visible = true)
+
 func on_devils_update(count: int) -> void:
 	%DevilsCount.text = str(count)
